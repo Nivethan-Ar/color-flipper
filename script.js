e422c3a8f2288bdbf10d48c745a42f99f781f894
Nivethan Ar <arnive19@gmail.com>
2021-08-22 11:54:06 +0530

    add border to main elements

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 
 const changeBtn = document.querySelector('.btn');
 const containerDiv = document.querySelector('.container');
-const colorText = document.querySelector('.color')
+const colorText = document.querySelector('.color');
+const pageHeadingText = document.querySelector('.page-heading');
 let randomColor;
-changeBtn.addEventListener('click', function () {
-	randomColor ='#'+ Math.floor(Math.random() * 16777215).toString(16);
+
+const colorFlip = function () {
+  randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 	containerDiv.style.backgroundColor = randomColor;
-  colorText.textContent = randomColor;
-  colorText.style.color = randomColor;
-});
+	colorText.textContent = randomColor;
+	colorText.style.color = randomColor;
+};
+
+changeBtn.addEventListener('click', colorFlip);
