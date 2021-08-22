eb1f48fede28d1ed810c6f8bc4decd4c8bab354b
Nivethan Ar <arnive19@gmail.com>
2021-08-22 12:46:47 +0530

    add stroke for better readability

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const containerDiv = document.querySelector('.container');
 const colorText = document.querySelector('.color');
 const pageHeadingText = document.querySelector('.page-heading');
 let randomColor;
-
 const colorFlip = function () {
-  randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+	randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 	containerDiv.style.backgroundColor = randomColor;
 	colorText.textContent = randomColor;
 	colorText.style.color = randomColor;
-  pageHeadingText.style.color =randomColor;
+	pageHeadingText.style.color = randomColor;
 };
+colorFlip();
 
 changeBtn.addEventListener('click', colorFlip);
